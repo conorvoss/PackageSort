73104d6e96386fd54bbdd9ff5fd8d14bc81abbf2
conorvoss <conorvoss7@gmail.com>
2025-02-18 17:16:01 -0800

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,3 +1,13 @@
 def sort(width, height, length, mass):
-  volume = width * height * length
+  if width <= 0 or height <= 0 or length <= 0 or mass <= 0:
+    return "Error: inputs must be greater than 0"
+    
+  bulky = width * height * length >= 1000000 or width >= 150 or height >= 150 or length >= 150
+  heavy = mass >= 20
   
+  if bulky and heavy:
+    return "REJECTED"
+  elif bulky or heavy:
+    return "SPECIAL"
+  else:
+    return "STANDARD"
